6044e3d5b30050d12a6bdb80f1cc5f947fb53514
Kali Muthu <kalimuthu@cygnussoftwares.com>
2025-03-21 12:45:59 +0530

    initial push

---
+++ AdvancedStudentApp/AdvancedStudentApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AdvancedStudentApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
